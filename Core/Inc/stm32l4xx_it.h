13052497772f41879cad19cf10e03580b0a17d0b
Ehsan Hosseini <127576777+ehsanh95@users.noreply.github.com>
2026-09-01 11:52:25 +0330

    radio: preserve CubeMX interrupt header sections [UNTESTED]

--- Core/Inc/stm32l4xx_it.h
+++ Core/Inc/stm32l4xx_it.h
@@ -8,10 +8,16 @@
   *
   * Copyright (c) 2026 STMicroelectronics.
   * All rights reserved.
-  ******************************************************************************
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+ ******************************************************************************
   */
 /* USER CODE END Header */
 
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32L4xx_IT_H
 #define __STM32L4xx_IT_H
 
@@ -19,8 +25,27 @@
  extern "C" {
 #endif
 
-#include "main.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
 void NMI_Handler(void);
 void HardFault_Handler(void);
 void MemManage_Handler(void);
@@ -30,9 +55,9 @@ void SVC_Handler(void);
 void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
-
-/* Radio DIO0 (PA10) is staged on EXTI15_10 for managed TX completion. */
+/* USER CODE BEGIN EFP */
 void EXTI15_10_IRQHandler(void);
+/* USER CODE END EFP */
 
 #ifdef __cplusplus
 }
