c5beadcd54c53b85b2a1d4428814d5cdea8333e1
Ehsan Hosseini <127576777+ehsanh95@users.noreply.github.com>
2026-09-04 23:35:51 +0330

    fix: preserve telemetry v1 and add BMA456 fields

--- App/Application/telemetry_data.h
+++ App/Application/telemetry_data.h
@@ -8,23 +8,46 @@ typedef struct
 {
     uint16_t sequence;
     uint16_t config_version;
-
     bool temperature_valid;
     int32_t temperature_current_mdeg_c;
     int32_t temperature_min_mdeg_c;
     int32_t temperature_max_mdeg_c;
     int32_t max_negative_excursion_mdeg_c;
+    bool motion_valid;
+    uint16_t motion_event_count;
+    uint16_t contraction_candidate_count;
+    uint16_t rotation_candidate_count;
+    bool inter_event_interval_valid;
+    uint16_t inter_event_interval_mean_s;
+    uint32_t inter_event_interval_variance_s2;
+    uint16_t rms_dynamic_accel_mg;
+    uint16_t peak_dynamic_accel_mg;
+    uint32_t combined_event_flags;
+    uint16_t battery_mv;
+    uint8_t battery_percent;
+    bool event_record_overflow;
+    bool fault_present;
+    bool health_degraded;
+    bool health_critical;
+} bolus_telemetry_summary_v1_t;
 
+typedef struct
+{
+    uint16_t sequence;
+    uint16_t config_version;
+    bool temperature_valid;
+    int32_t temperature_current_mdeg_c;
+    int32_t temperature_min_mdeg_c;
+    int32_t temperature_max_mdeg_c;
+    int32_t max_negative_excursion_mdeg_c;
     bool motion_valid;
     uint16_t episode_count;
     uint16_t accepted_pulse_count;
     uint16_t suppressed_pulse_count;
     uint16_t max_pulses_per_episode;
-
     bool inter_pulse_interval_valid;
     uint16_t inter_pulse_interval_mean_s;
     uint32_t inter_pulse_interval_variance_s2;
-
     bool mpu_valid;
     uint16_t mpu_burst_count;
     uint16_t rms_dynamic_accel_mg;
@@ -33,20 +56,17 @@ typedef struct
     uint16_t peak_angular_velocity_dps;
     uint32_t total_angular_motion_cdeg;
     uint16_t max_orientation_change_cdeg;
-
     uint16_t contraction_candidate_count;
     uint16_t rotation_candidate_count;
     uint32_t combined_event_flags;
-
     uint16_t battery_mv;
     uint8_t battery_percent;
-
     bool fault_present;
     bool health_degraded;
     bool health_critical;
     bool staging_untested;
 
-    /* BMA456 native telemetry. Do not populate from MPU6050. */
+    /* Native BMA456 data only. Never sourced from MPU6050. */
     bool bma456_valid;
     uint32_t bma_step_count;
     int16_t bma_accel_x_mg;
